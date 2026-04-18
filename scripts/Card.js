c64f1129c01a43b2d225d52aba1adf57bfa6d88e
ohara <cristianohara@gmail.com>
2026-04-18 13:54:50 -0300

    classe card

---
+++ scripts/Card.js
@@ -0,0 +1,64 @@
+class Card {
+
+constructor (text, link, cardSelector) {
+
+this._text = text;
+this._link = link;
+this._cardSelector = cardSelector;
+}
+
+_getTemplate() {
+  const cardElement = document
+    .querySelector(this._cardSelector)
+    .content
+    .querySelector(".card")
+    .cloneNode(true);
+
+  return cardElement;
+}
+generateCard() {
+  this._element = this._getTemplate();
+
+  const cardImage = this._element.querySelector(".card__image")
+
+  const cardTitle = this._element.querySelector(".card__title");
+
+  cardImage.src = this._link ;
+
+  cardImage.alt = this._text ;
+
+  cardTitle.textContent = this._text ;
+
+  this._setEventListeners();
+
+  return this._element;
+}
+
+_setEventListeners() {
+    this._element
+      .querySelector(".card__like-button")
+      .addEventListener("click", () => {
+        this._handleLikeIcon(); // Chama a gaveta do Like
+      });
+
+    this._element
+      .querySelector(".card__delete-button")
+      .addEventListener("click", () => {
+        this._handleDeleteCard(); // Chama a gaveta do Delete
+      });
+  }
+
+  // Gaveta 2: O que acontece no Like
+  _handleLikeIcon() {
+    this._element
+      .querySelector(".card__like-button")
+      .classList.toggle("card__like-button_is-active");
+  }
+
+  // Gaveta 3: O que acontece no Delete
+  _handleDeleteCard() {
+    this._element.remove();
+    this._element = null;
+  }
+  }
+
